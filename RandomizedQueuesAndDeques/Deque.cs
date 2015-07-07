0f4d87dc467e6645ce8fad46c2bac505c6fecbd6
Anton <apolkhovskiy@dataart.com>
2015-07-07 18:02:34 +0300

    adding Deque

---
+++ RandomizedQueuesAndDeques/Deque.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RandomizedQueuesAndDeques
+{
+    public class Deque<T> : IEnumerable<T>
+    {
+        private int N;
+        private Node _first;
+        private Node _last;
+
+        public Deque()
+        {
+            _first = null;
+            _last = null;
+            N = 0;
+        }
+
+        public bool IsEmpty
+        {
+            get { return N == 0; }
+        }
+
+        public int Size
+        {
+            get { return N; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new ListIterator(_first);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void AddFirst(T item)
+        {
+            if (Equals(item, null))
+            {
+                throw new NullReferenceException("item cannot be null");
+            }
+
+            var oldFirst = _first;
+            _first = new Node {Item = item, Previous = null, Next = oldFirst};
+            if (IsEmpty)
+            {
+                _last = _first;
+            }
+            else
+            {
+                oldFirst.Previous = _first;
+            }
+
+            N += 1;
+        }
+
+        public void AddLast(T item)
+        {
+            if (Equals(item, null))
+            {
+                throw new NullReferenceException("item cannot be null");
+            }
+
+            var oldLast = _last;
+            _last = new Node {Item = item, Previous = oldLast, Next = null};
+            if (IsEmpty)
+            {
+                _first = _last;
+            }
+            else
+            {
+                oldLast.Next = _last;
+            }
+
+            N += 1;
+        }
+
+        public T RemoveFirst()
+        {
+            if (IsEmpty)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var item = _first.Item;
+            _first = _first.Next;
+            N -= 1;
+
+            if (IsEmpty)
+            {
+                _last = null;
+            }
+            else
+            {
+                _first.Previous = null;
+            }
+
+            return item;
+        }
+
+        public T RemoveLast()
+        {
+            if (IsEmpty)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var item = _last.Item;
+            _last = _last.Previous;
+            N -= 1;
+
+            if (IsEmpty)
+            {
+                _first = null;
+            }
+            else
+            {
+                _last.Next = null;
+            }
+
+            return item;
+        }
+
+        private class ListIterator : IEnumerator<T>
+        {
+            private readonly Node _first;
+            private Node _current;
+
+            public ListIterator(Node first)
+            {
+                _current = first;
+                _first = first;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                return _current != null;
+            }
+
+            public void Reset()
+            {
+                _current = _first;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    var oldCurrent = _current;
+                    _current = _current.Next;
+                    return oldCurrent.Item;
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+        }
+
+        internal class Node
+        {
+            public T Item { get; set; }
+            public Node Next { get; set; }
+            public Node Previous { get; set; }
+        }
+    }
+}
---
+++ RandomizedQueuesAndDeques/DequeTests.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace RandomizedQueuesAndDeques
+{
+    internal class DequeTests
+    {
+        [Test]
+        public void TestDequeFirst()
+        {
+            var deque = new Deque<String>();
+            var items = new List<string>
+            {
+                "0",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9"
+            };
+
+            Assert.AreEqual(0, deque.Size);
+            Assert.IsTrue(deque.IsEmpty);
+
+            foreach (var item in items)
+            {
+                deque.AddFirst(item);
+            }
+
+            var builder = new StringBuilder();
+            foreach (var item in deque)
+            {
+                builder.Append(item);
+            }
+
+            Assert.AreEqual("9876543210", builder.ToString());
+            Assert.AreEqual(10, deque.Size);
+            Assert.IsFalse(deque.IsEmpty);
+
+            while (!deque.IsEmpty)
+            {
+                deque.RemoveFirst();
+            }
+
+            Assert.AreEqual(0, deque.Size);
+            Assert.IsTrue(deque.IsEmpty);
+        }
+
+        [Test]
+        public void TestDequeLast()
+        {
+            var deque = new Deque<String>();
+            var items = new List<string>
+            {
+                "0",
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9"
+            };
+
+            Assert.AreEqual(0, deque.Size);
+            Assert.IsTrue(deque.IsEmpty);
+
+            foreach (var item in items)
+            {
+                deque.AddLast(item);
+            }
+
+            var builder = new StringBuilder();
+            foreach (var item in deque)
+            {
+                builder.Append(item);
+            }
+
+            Assert.AreEqual("0123456789", builder.ToString());
+            Assert.AreEqual(10, deque.Size);
+            Assert.IsFalse(deque.IsEmpty);
+
+            while (!deque.IsEmpty)
+            {
+                deque.RemoveLast();
+            }
+
+            Assert.AreEqual(0, deque.Size);
+            Assert.IsTrue(deque.IsEmpty);
+        }
+
+        [Test]
+        public void AdvancedTestDequeFirst()
+        {
+            var deque = new Deque<String>();
+            var items = new List<string>
+            {
+                "0",
+                "1",
+                "2",
+                "-",
+                "-",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9"
+            };
+
+            Assert.AreEqual(0, deque.Size);
+            Assert.IsTrue(deque.IsEmpty);
+
+            foreach (var item in items)
+            {
+                if (item != "-")
+                {
+                    deque.AddFirst(item);
+                }
+                else
+                {
+                    if (!deque.IsEmpty)
+                    {
+                        deque.RemoveFirst();
+                    }
+                }
+            }
+
+            var builder = new StringBuilder();
+            foreach (var item in deque)
+            {
+                builder.Append(item);
+            }
+
+            Assert.AreEqual("98765430", builder.ToString());
+            Assert.AreEqual(8, deque.Size);
+            Assert.IsFalse(deque.IsEmpty);
+
+            while (!deque.IsEmpty)
+            {
+                deque.RemoveFirst();
+            }
+
+            Assert.AreEqual(0, deque.Size);
+            Assert.IsTrue(deque.IsEmpty);
+        }
+
+        [Test]
+        public void AdvancedTestDequeLast()
+        {
+            var deque = new Deque<String>();
+            var items = new List<string>
+            {
+                "0",
+                "1",
+                "2",
+                "-",
+                "-",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9"
+            };
+
+            Assert.AreEqual(0, deque.Size);
+            Assert.IsTrue(deque.IsEmpty);
+
+            foreach (var item in items)
+            {
+                if (item != "-")
+                {
+                    deque.AddLast(item);
+                }
+                else
+                {
+                    if (!deque.IsEmpty)
+                    {
+                        deque.RemoveLast();
+                    }
+                }
+            }
+
+            var builder = new StringBuilder();
+            foreach (var item in deque)
+            {
+                builder.Append(item);
+            }
+
+            Assert.AreEqual("03456789", builder.ToString());
+            Assert.AreEqual(8, deque.Size);
+            Assert.IsFalse(deque.IsEmpty);
+
+            while (!deque.IsEmpty)
+            {
+                deque.RemoveLast();
+            }
+
+            Assert.AreEqual(0, deque.Size);
+            Assert.IsTrue(deque.IsEmpty);
+        }
+    }
+}
